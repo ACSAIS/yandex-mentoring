6881f78d271256accc755e06e2f34bfca395f01f
Georgy Klyukovkin <georgy.klyukovkin@nordigy.ru>
2020-04-28 15:40:55 +0300

    refactoring

--- templates_webinar/script.js
+++ templates_webinar/script.js
@@ -8,7 +8,15 @@ const todos = [
 ];
 const itemTemplate = document.querySelector('.item_template').content;
 const list = document.querySelector('.list');
+const formButton = document.querySelector('.form__submit');
+const formInput = document.querySelector('.form__input');
 
+{/* <li class="list__item">
+    <span class="item__text"></span>
+    <img class="edit" src="images/Edit.png" alt="Редактировать">
+    <img class="duplicate" src="images/Duplicate.png" alt="Копировать">
+    <img class="delete" src="images/Delete.png" alt="Удалить">
+</li> */}
 function render() {
     
 }
