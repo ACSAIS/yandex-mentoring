3c4dbac84d01d02d57e5461acea5fbe577950ed9
Georgy Klyukovkin <georgy.klyukovkin@nordigy.ru>
2020-04-28 14:32:54 +0300

    inital commit

---
+++ templates_webinar/script.js
@@ -0,0 +1,15 @@
+const todos = [
+    'Сделать проектную работу', 
+    'Полить цветы', 'Пройти туториал по Реакту', 
+    'Сделать фронт для своего проекта', 
+    'Погулять с собакой', 
+    'Разобраться в замыканиях', 
+    'Решить задачу на Codewars',
+];
+const itemTemplate = document.querySelector('.item_template').content;
+const list = document.querySelector('.list');
+
+function render() {
+    
+}
+render();
